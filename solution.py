e34701745cc85381bde35bcba53b53037d164728
Qingyang Li (Mike) <mliqingyang@gmail.com>
2022-08-10 17:12:10 +1000

    change to functions

--- solution.py
+++ solution.py
@@ -4,20 +4,30 @@ import re
 #The number of unique IP addresses
 #The top 3 most visited URLs
 #The top 3 most active IP addresses
+
+#open file
 f = open('programming-task-example-data_(1).log', 'r')
 fline = f.readlines()
-p = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+def uniq_ip(filename):
+    f = open(filename, 'r')
+    ip_list=[]
+    # pattern for ip address, from 0.0.0.0 to 255.255.255.255
+    p = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' 
 
-ip_list=[]
-ip_dic={}
+    for line in fline:
+        ip_address = re.findall(p,line)
+        if ip_address not in ip_list:
+            ip_list.append(ip_address)    
 
-for line in fline:
-    ip_address = p.search(line)[0]
-    if ip_address not in ip_list:
-        ip_list.append(ip_address)    
-
-
-print(len(ip_list))
+    #close file
+    f.close
+    return len(ip_list)
 
 
-f.close
+if __name__ == '__main__':
+    n_uniip = uniq_ip('programming-task-example-data_(1).log')
+
+
+    print('The number of unique IP addresses is :',n_uniip,'\n',
+            'The top 3 most visited URLs are: ','\n',
+            'The top 3 most active IP addresses are: ', '\n')
