a5e16696979a9bca8549d5f55b2ca4e142caf115
Qingyang Li (Mike) <mliqingyang@gmail.com>
2022-08-10 22:56:38 +1000

    top 3 most active IP addresses

--- solution.py
+++ solution.py
@@ -2,16 +2,15 @@
 import re
 
 #The number of unique IP addresses
-
-f = open('programming-task-example-data_(1).log', 'r')
-fline = f.readlines()
 def uniq_ip(filename):
     f = open(filename, 'r')
+    fline = f.readlines()
     ip_list=[]
     # pattern for ip address, from 0.0.0.0 to 255.255.255.255
     # seprate to 3 groups 0-199, 200-249,250-255
     p = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
     for line in fline:
+        # list_ip_address is a list of tuple
         list_ip_address = re.findall(p,line)
         # append to list if the ip has not been seen
         for ip in list_ip_address:
@@ -25,15 +24,29 @@ def uniq_ip(filename):
 #The top 3 most visited URLs
 def v_url(filename):
     f = open(filename, 'r')
-    url_list = []
+    fline = f.readlines()
+    url_dic = {}
+    p = r'https?://\S+|\bGET\s\S+'
+    for line in fline:
+        #url string after word 'GET'
+        url_str = re.search(p,line).group().split()[1]
+        if url_str not in url_dic:
+            url_dic[url_str] = 1
+        else:
+            url_dic[url_str] += 1
+    #sort url dictionary
+    url_dic = sorted(url_dic.items(), key=lambda x: x[1], reverse=True)
+
     f.close
-    return url_list
+    return key_tuple(url_dic)[0:3]
 
 #The top 3 most active IP addresses
 def active_ip(filename):
     f = open(filename, 'r')
+    fline = f.readlines()
     #A dictionary, IP as key, number of appearance as value
     ip_dict = {}
+    # IP address pattern
     p = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
     for line in fline:
          ip_address = re.findall(p,line)
@@ -46,17 +59,17 @@ def active_ip(filename):
     #sorted list of key value tuples
     ip_dict = sorted(ip_dict.items(), key=lambda x: x[1], reverse=True)
 
-    # take the first element in the tuple which is the key(IP address)
-    ip_list = []
-    for kv_tuple in ip_dict:
-        ip_list.append(kv_tuple[0])
-  
     #close file
     f.close
-    return join_tuple(ip_list[0:3])
+    return join_tuple(key_tuple(ip_dict)[0:3])
 
-
-
+# Extract keys from a list of key value tuple
+def key_tuple(tuple_list):
+    l = []
+    # take the first element in the tuple which is the key
+    for kv_tuple in tuple_list:
+        l.append(kv_tuple[0])
+    return l
 # convert tuple of string into IP address format
 def join_tuple(tup_list):
     l = []
